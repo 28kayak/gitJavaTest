bbd376576dd8bfbe81141d3e6e14de364cb9fd8a
kayak28 <kaya_ota@msn.com>
2013-01-20 12:20:01 -0800

    try array counting only odd number

--- Array.java
+++ Array.java
@@ -3,24 +3,36 @@ public class Array
 	public static void main(String [] args)
 	{
 		int array[] = {1,2,3,4,5,6,7,8};
-		/*for(int i = 0; i > array.length; i = 2*i+1)
+		/*for(int i = 1; i <= array.length; i = 2*i+1)
 		{
 			System.out.println(array[i]);
 		}
 	
 		*/
-		int i = 0;
-		while(i >= array.length)
+		int addition1;
+		
+		int i = 1;
+		while(i <= array.length)
 		{
-			System.out.println(array[i]);
+			//i = 2*i+1;
+			System.out.println("i ="+ i);
+			System.out.println("array[i] =" +array[i]);
+			addition1 =+ array[i];
+			System.out.println("addition1 ="+ addition1);
 			i = 2*i+1;
-			System.out.println(i);
+			
+			/*System.out.println("before addition" + array[i]);
+			addition1 =+ array[i];
+			
+			System.out.println("after addition"+ addition1);
+			System.out.println("i ="+ i);
+			*/
 	
 		
 		
 		
 		}
-	
+		
 	}
 
 
